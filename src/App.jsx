3cac4d77e75ff7e0f6f4f0ebe9230f60ec953f5b
Yaren Çetinkaya <cetinkayaa.yarenn@gmail.com>
2023-08-09 15:15:58 +0300

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { WagmiConfig, createConfig, mainnet } from 'wagmi'
-import { createPublicClient, http } from 'viem'
-import Profile from "./components/Profile"
+import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { createPublicClient, http } from "viem";
+import Profile from "./components/Profile";
+import Header from "./components/Header";
 
 const config = createConfig({
   autoConnect: true,
   publicClient: createPublicClient({
     chain: mainnet,
-    transport: http()
+    transport: http(),
   }),
-})
+});
 
 export default function App() {
   return (
-    <WagmiConfig config={config}>
-      <Profile />
-    </WagmiConfig>
-  )
+    <>
+      <Header />
+      <br className="p-10" />
+      <WagmiConfig config={config}>
+        <Profile />
+      </WagmiConfig>
+    </>
+  );
 }
